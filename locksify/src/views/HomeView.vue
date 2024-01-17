1051eac9fa57a42ab7e524b7880239fc954784f9
Adrijana1308 <adrijanababic4@gmail.com>
2024-01-17 21:14:01 +0100

    Update HomeView.vue

--- locksify/src/views/HomeView.vue
+++ locksify/src/views/HomeView.vue
@@ -6,10 +6,31 @@
       <div ref="ball2" class="shadow ball ball2"></div>
     </div>
     <div class="search-container">
-      <form action="/" method="get">
-        <input type="text" id="search-box" placeholder="Search...">
-        <button type="submit" id="search-button"><i class="bi bi-search"></i></button>
-      </form>
+       <div class="input-container"> 
+          <form class="form" action="/" method="get">
+            <i class="bi bi-search"></i>
+            <input type="text" class="search-box" placeholder="Search...">
+          </form>
+        </div>
+        <div class="input-container"> 
+          <form class="form" action="/" method="get">
+            <i class="bi bi-search"></i>
+            <input type="text" class="search-box" placeholder="Search...">
+          </form>
+        </div>
+        <div class="input-container"> 
+          <form class="form" action="/" method="get">
+            <i class="bi bi-search"></i>
+            <input type="text" class="search-box" placeholder="Search...">
+          </form>
+        </div>
+        <div class="input-container"> 
+          <form class="form" action="/" method="get">
+            <i class="bi bi-search"></i>
+            <input type="text" class="search-box" placeholder="Search...">
+          </form>
+        </div>
+        <button type="submit" class="search-button">Search</button>
     </div>
   </div>
 </template>
@@ -99,26 +120,43 @@ body {
 }
 .search-container {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-#search-box {
-  border: 10px solid #d890f530;
-  border-radius: 50px 0 0 50px;
+  gap: 10px;
+  width: 60%;
   height: 70px;
-  padding: 10px;
-  font-size: 17px;
-  color: #2c3e50;
-  width: 1040px;
+  margin: auto;
+  border: 10px solid #d890f530;
+  border-radius: 50px;
+  padding: 5px;
+  justify-content: space-evenly;
+  align-items: center;
 }
 
-#search-box::placeholder {
+.input-container{
+  width: 200px;
+  height: 60px;
+}
+
+.form{
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.search-box {
+  border: none;
+  height: 80%;
+  width: 80%;
+  margin-left: 10px;
+}
+
+.search-box::placeholder {
   color: #2c3e50;
 }
 
-#search-button {
+.search-button {
   background-color: #2c3e50;
   border: none;
   border-radius: 0 50px 50px 0;
@@ -126,7 +164,7 @@ body {
   padding: 0 20px;
 }
 
-#search-button:hover {
+.search-button:hover {
   background-color: white;
 }
 
