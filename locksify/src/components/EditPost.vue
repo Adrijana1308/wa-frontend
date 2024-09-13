092522a7c9f5f6a55eb8aa78372b3ed856e8cd85
Ivan Sviben <ivan.sviben97@gmail.com>
2024-09-14 00:26:01 +0200

    npm run build

--- locksify/src/components/EditPost.vue
+++ locksify/src/components/EditPost.vue
@@ -2,62 +2,22 @@
   <div class="edit-post">
     <h1>Edit Post</h1>
     <form @submit.prevent="submitPost">
-      <input
-        v-model="editedPost.name"
-        type="text"
-        placeholder="Salon Name"
-        required
-      />
-      <input
-        v-model="editedPost.location"
-        type="text"
-        placeholder="Location"
-        required
-      />
-      <input
-        v-model="editedPost.open"
-        type="time"
-        placeholder="Open Time"
-        required
-      />
-      <input
-        v-model="editedPost.close"
-        type="time"
-        placeholder="Close Time"
-        required
-      />
+      <input v-model="editedPost.name" type="text" placeholder="Salon Name" required />
+      <input v-model="editedPost.location" type="text" placeholder="Location" required />
+      <input v-model="editedPost.open" type="time" placeholder="Open Time" required />
+      <input v-model="editedPost.close" type="time" placeholder="Close Time" required />
       <input v-model="editedPost.source" type="text" placeholder="Image URL" />
 
       <div class="hairstyles">
         <h3>Hairstyles</h3>
         <div v-for="(type, index) in hairTypes" :key="index">
           <h4>{{ type.charAt(0).toUpperCase() + type.slice(1) }}</h4>
-          <div
-            v-for="(hairstyle, idx) in editedPost.hairstyles[type]"
-            :key="idx"
-          >
-            <input
-              v-model="hairstyle.type"
-              type="text"
-              placeholder="Type"
-              required
-            />
-            <input
-              v-model.number="hairstyle.price"
-              type="number"
-              placeholder="Price"
-              required
-            />
-            <input
-              v-model.number="hairstyle.duration"
-              type="number"
-              placeholder="Duration (minutes)"
-              required
-            />
+          <div v-for="(hairstyle, idx) in editedPost.hairstyles[type]" :key="idx">
+            <input v-model="hairstyle.type" type="text" placeholder="Type" required />
+            <input v-model.number="hairstyle.price" type="number" placeholder="Price" required />
+            <input v-model.number="hairstyle.duration" type="number" placeholder="Duration (minutes)" required />
           </div>
-          <button type="button" @click="addHairstyle(type)">
-            Add {{ type }} Hairstyle
-          </button>
+          <button type="button" @click="addHairstyle(type)">Add {{ type }} Hairstyle</button>
         </div>
       </div>
 
@@ -67,60 +27,58 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import { mapActions } from "vuex"; // Import mapActions to handle Vuex actions
-import { Posts } from "../Services/index.js";
+import { ref, onMounted } from 'vue';
+import { mapActions } from 'vuex'; // Import mapActions to handle Vuex actions
+import { Posts } from '../Services/index.js';
+import { useRouter } from "vue-router";
 
 export default {
-  name: "EditPost",
+  name: 'EditPost',
   props: {
     postId: {
       type: String,
-      required: true,
-    },
+      required: true
+    }
   },
   setup(props, { emit }) {
     const editedPost = ref({
-      name: "",
-      location: "",
-      open: "",
-      close: "",
-      source: "",
+      name: '',
+      location: '',
+      open: '',
+      close: '',
+      source: '',
       hairstyles: {
         short: [],
         medium: [],
         long: [],
-        other: [],
-      },
+        other: []
+      }
     });
 
-    const hairTypes = ["short", "medium", "long", "other"];
+    const hairTypes = ['short', 'medium', 'long', 'other'];
+    const router = useRouter();
 
     const fetchPostDetails = async () => {
       try {
         const postData = await Posts.getPostById(props.postId);
         editedPost.value = postData;
       } catch (error) {
-        console.error("Error fetching post details:", error);
+        console.error('Error fetching post details:', error);
       }
     };
 
     const addHairstyle = (type) => {
-      editedPost.value.hairstyles[type].push({
-        type: "",
-        price: 0,
-        duration: 0,
-      });
+      editedPost.value.hairstyles[type].push({ type: '', price: 0, duration: 0 });
     };
 
     const submitPost = async () => {
       try {
         const response = await Posts.updatePost(props.postId, editedPost.value);
-        console.log("Post updated successfully:", response.data);
-        emit("postUpdated", response); // Emit event after successful update
-        router.push("/");
+        console.log('Post updated successfully:', response.data);
+        emit('postUpdated', response); // Emit event after successful update
+        router.push({name: 'home'});
       } catch (error) {
-        console.error("Error submitting post:", error);
+        console.error('Error submitting post:', error);
       }
     };
 
@@ -134,69 +92,70 @@ export default {
       addHairstyle,
       submitPost,
     };
-  },
+  }
 };
 </script>
-
-<style scoped>
-.edit-post {
-  max-width: 600px;
-  margin: 140px auto;
-  padding: 20px;
-  background: blanchedalmond;
-  border: 10px solid #d890f530;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-form {
-  margin: 20px;
-  display: flex;
-  flex-direction: column;
-}
-
-input {
-  margin-bottom: 10px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-button {
-  margin: 7px;
-  padding: 10px;
-  background: #d890f530;
-  color: black;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-button:hover {
+  
+  <style scoped>
+  .edit-post {
+    max-width: 600px;
+    margin: 140px auto;
+    padding: 20px;
+    background: blanchedalmond;
+    border: 10px solid #d890f530;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  form {
+    margin: 20px;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  input {
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  button {
+    margin: 7px;
+    padding: 10px;
+    background: #d890f530;
+    color: black;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button:hover {
   color: rgba(0, 0, 0, 1);
   transform: scale(1.1);
-  transition: 0.1s ease-in-out;
+  transition: .1s ease-in-out;
 }
-
-.hairstyles {
-  margin-top: 20px;
-}
-
-.hairstyles h4 {
-  margin-bottom: 5px;
-}
-
-.hairstyles div {
-  display: flex;
-  flex-direction: column;
-}
-
-.hairstyles div input {
-  margin-bottom: 5px;
-}
-
-.hairstyles button {
-  align-self: flex-start;
-  margin-top: 10px;
-}
-</style>
+  
+  .hairstyles {
+    margin-top: 20px;
+  }
+  
+  .hairstyles h4 {
+    margin-bottom: 5px;
+  }
+  
+  .hairstyles div {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .hairstyles div input {
+    margin-bottom: 5px;
+  }
+  
+  .hairstyles button {
+    align-self: flex-start;
+    margin-top: 10px;
+  }
+  </style>
+  
